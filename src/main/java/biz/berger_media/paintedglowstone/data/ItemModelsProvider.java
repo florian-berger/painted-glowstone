552a8839e2502a17ce4056d5ad5e2eb19f27c5aa
Florian Berger <dev@florian-berger.info>
2022-11-14 22:22:41 +0100

    Prepared for resource generation

---
+++ src/main/java/biz/berger_media/paintedglowstone/data/ItemModelsProvider.java
@@ -0,0 +1,19 @@
+package biz.berger_media.paintedglowstone.data;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.moddingx.libx.annotation.data.Datagen;
+import org.moddingx.libx.datagen.provider.ItemModelProviderBase;
+import org.moddingx.libx.mod.ModX;
+
+@Datagen
+public class ItemModelsProvider extends ItemModelProviderBase {
+    public ItemModelsProvider(ModX mod, DataGenerator generator, ExistingFileHelper fileHelper) {
+        super(mod, generator, fileHelper);
+    }
+
+    @Override
+    protected void setup() {
+        // Nothing special to setup here
+    }
+}
